2c17234b0e184d957507b54defd3aec08f0f7579
Sweets Sweetman <sweetmantech@gmail.com>
2023-08-27 10:39:12 -0300

    I fix build errors.

--- hooks/usePWADownload.tsx
+++ hooks/usePWADownload.tsx
@@ -6,12 +6,13 @@ const usePWADownload = () => {
   useEffect(() => {
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
     const isStandalone =
-      window.matchMedia("(display-mode: standalone)").matches || window.navigator.standalone
+      window.matchMedia("(display-mode: standalone)").matches ||
+      (window.navigator as any).standalone
 
     if (isMobile && !isStandalone) {
       setShowModal(true)
     }
-  }, []) // The empty dependency array ensures this useEffect only runs once, after the component mounts.
+  }, [])
 
   return {
     showModal,
